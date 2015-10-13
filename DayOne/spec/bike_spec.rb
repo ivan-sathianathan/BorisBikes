13985e4f1d4357945652baa41a1f86ea9ffb08b5
octavian rotari <octavianrotari@octavians-MacBook-Pro.local>
2015-10-13 16:10:24 +0100

    stage 1 requirements boris bikes

--- DayOne/spec/bike_spec.rb
+++ DayOne/spec/bike_spec.rb
@@ -1,6 +1,4 @@
 require 'bike'
 describe Bike do
-  it "respond to working? method" do
-    expect(subject).to respond_to :working?
-  end
+  it {is_expected.to respond_to :working?}
 end
--- DayOne/spec/docking_station_spec.rb
+++ DayOne/spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station.rb'
 describe DockingStation do
-  it "respond to release bike" do
-    expect(subject).to respond_to :release_bike
+  it { is_expected.to respond_to :release_bike}
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
